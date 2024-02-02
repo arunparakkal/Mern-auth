b8ed5fd04a7efc7af74e5ab7ae22e70abee2d551
Arun S <arunaru0034@gmail.com>
2024-02-02 15:46:44 +0530

    create

--- client/src/assets/pages/Home.jsx
+++ client/src/assets/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-console.log("i m");
+
 export default function Home() {
-    {console.log("hello world")}
+   
   return (
     <div>
-        {console.log("hello world")}
+   
       <h1 className='text-red-900'>HOme</h1>
     </div>
   )
