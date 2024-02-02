b09043b1833c816e1ef8a6d62f3e46184d099810
Arun S <arunaru0034@gmail.com>
2024-02-02 14:05:57 +0530

    create page and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,20 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route,  } from "react-router-dom";
+import Home from "./assets/pages/Home";
+import Profile from "./assets/pages/Profile";
+import SignIn from "./assets/pages/SignIn";
+import SignUp from "./assets/pages/SignUp";
+import About from "./assets/pages/About";
 
 export default function App() {
-  return <h1 className='text-red-600'>App</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="sign-in" element={<SignIn />} />
+        <Route path="sign-up" element={<SignUp />} />
+        <Route path="about" element={<About/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ client/src/assets/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+console.log("i m");
+export default function Home() {
+    {console.log("hello world")}
+  return (
+    <div>
+        {console.log("hello world")}
+      <h1 className='text-red-900'>HOme</h1>
+    </div>
+  )
+}
